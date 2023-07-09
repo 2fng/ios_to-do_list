94e4e98988fe9e37cf3dda6ad1d4fd50f856cd79
Hua Son Tung <hua.son.tung@sun-asterisk.com>
2023-07-09 15:26:25 +0700

    Initial Commit

---
+++ ios_to-do_list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_to-do_list
+//
+//  Created by Hua Son Tung on 09/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
